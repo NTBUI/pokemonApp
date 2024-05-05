9cff9c22a581de11cc01c1c12e49286c2bff02cc
NTBUI <126556075+NTBUI@users.noreply.github.com>
2024-05-05 00:40:26 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// StAuth10244: I Ngoc Bui, 000899963 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
 import React, { useEffect, useState } from 'react';
 import { StatusBar, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, TextInput} from 'react-native';
 import axios from 'axios';
